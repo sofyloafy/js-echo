b84de6c7dd5f560e9bf641f2b2e44b35dd245c1f
Sophie <sophiebrown438@gmail.com>
2020-08-19 19:30:13 +0100

    Attempt to include user input

--- src/echo.js
+++ src/echo.js
@@ -1,7 +1,24 @@
 class Echo {
 
-  repeat(words){
-    return words
+
+  promptEcho(){
+    const readline = require("readline");
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    
+    rl.question("What is your name ? ", function(name) {
+        rl.question("Where do you live ? ", function(country) {
+            console.log(`${name}, is a citizen of ${country}`);
+            rl.close();
+        });
+    });
+    
+    rl.on("close", function() {
+        console.log("\nBYE BYE !!!");
+        process.exit(0);
+    });
   }
 
 }
